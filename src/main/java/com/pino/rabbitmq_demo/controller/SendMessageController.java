6075b87e9951443e6e0d80fc9b53d80db57d713c
PinXian53 <p136301@gmail.com>
2021-07-25 11:32:53 +0800

    feat: add swagger dependency

--- src/main/java/com/pino/rabbitmq_demo/controller/SendMessageController.java
+++ src/main/java/com/pino/rabbitmq_demo/controller/SendMessageController.java
@@ -1,13 +1,18 @@
 package com.pino.rabbitmq_demo.controller;
 
 import com.pino.rabbitmq_demo.constant.QueueConst;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/testQueue")
+@Api(tags = "測試用")
 public class SendMessageController {
 
     private final RabbitTemplate rabbitTemplate;
@@ -16,8 +21,9 @@ public class SendMessageController {
         this.rabbitTemplate = rabbitTemplate;
     }
 
-    @GetMapping("/putMessage/{msg}")
-    public ResponseEntity<String> putMessage(@PathVariable String msg) {
+    @ApiOperation(value = "接收訊息")
+    @PostMapping("/{msg}")
+    public ResponseEntity<String> sendMessage(@PathVariable String msg) {
         rabbitTemplate.convertAndSend(QueueConst.TEST_QUEUE, msg);
         return ResponseEntity.accepted().body("message 接收成功");
     }
